d1a80cfb410945546f4cbac36cd4393166078270
Junior Dering <juniordering@hotmail.com>
2026-01-04 10:18:51 -0300

    feat: Implement user registration endpoint with input validation and database persistence.

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -1,11 +1,78 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/database"
+	"api/src/models"
+	"api/src/repositories"
+	"api/src/responses"
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/go-sql-driver/mysql"
+)
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Create User"))
+	if r.Method != http.MethodPost {
+		responses.Error(w, http.StatusMethodNotAllowed, errors.New("Method not allowed"))
+		return
+	}
+
+	requestBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		responses.Error(w, http.StatusUnprocessableEntity, errors.New("Failed to read body request"))
+		return
+	}
+
+	var user models.User
+	if err = json.Unmarshal(requestBody, &user); err != nil {
+		responses.Error(w, http.StatusBadRequest, errors.New("Error converting user to struct"))
+		return
+	}
+
+	if err = user.Prepare("registration"); err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
+	db, err := database.Connect()
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, errors.New("Error connecting to database"))
+		return
+	}
+	defer db.Close()
+
+	repository := repositories.NewUsersRepository(db)
+	insertedID, err := repository.Create(user)
+	if err != nil {
+		var driverErr *mysql.MySQLError
+		if errors.As(err, &driverErr) && driverErr.Number == 1062 {
+			if strings.Contains(driverErr.Message, "email") {
+				responses.Error(w, http.StatusConflict, errors.New("email already registered"))
+				return
+			}
+			if strings.Contains(driverErr.Message, "nick") {
+				responses.Error(w, http.StatusConflict, errors.New("nick already registered"))
+				return
+			}
+		}
+
+		responses.Error(w, http.StatusInternalServerError, errors.New("Error creating user"))
+		return
+	}
+	user.ID = int(insertedID)
+
+	response := struct {
+		Message string `json:"message"`
+		ID      int    `json:"id"`
+	}{
+		Message: "User inserted",
+		ID:      user.ID,
+	}
+
+	responses.JSON(w, http.StatusCreated, response)
 }
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
--- src/models/user.go
+++ src/models/user.go
@@ -1,6 +1,12 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+
+	"github.com/badoux/checkmail"
+)
 
 type User struct {
 	ID        int       `json:"id,omitempty"`
@@ -10,4 +16,39 @@ type User struct {
 	Password  string    `json:"password,omitempty"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func (user *User) Prepare(step string) error {
+	if err := user.validate(step); err != nil {
+		return err
+	}
+
+	user.format()
+	return nil
+}
+
+func (user *User) validate(step string) error {
+	if user.Name == "" {
+		return errors.New("name is required and cannot be empty")
+	}
+
+	if user.Nick == "" {
+		return errors.New("nick is required and cannot be empty")
+	}
+
+	if err := checkmail.ValidateFormat(user.Email); err != nil {
+		return errors.New("the email entered is invalid")
+	}
+
+	if step == "registration" && user.Password == "" {
+		return errors.New("password is required and cannot be empty")
+	}
+
+	return nil
+}
+
+func (user *User) format() {
+	user.Name = strings.TrimSpace(user.Name)
+	user.Nick = strings.TrimSpace(user.Nick)
+	user.Email = strings.TrimSpace(user.Email)
 }
